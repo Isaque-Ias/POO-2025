d3a9bcde7ef42a0aace1a7bc8c138b1cb7ef2b71
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2025-09-18 20:52:01 -0300

    lista 9 streamlit

---
+++ poo_lista_09/index.py
@@ -0,0 +1,9 @@
+from templates.manterclienteUI import ManterClienteUI
+from templates.manterservicoUI import ManterServicoUI
+
+class IndexUI:
+    def main():
+        ManterClienteUI.main()
+        ManterServicoUI.main()
+
+IndexUI.main()
