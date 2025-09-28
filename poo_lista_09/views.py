1f40f2a481297f9ad72f13106aac21815bb22c81
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2025-09-28 19:03:52 -0300

    Horarios

--- poo_lista_09/views.py
+++ poo_lista_09/views.py
@@ -1,10 +1,14 @@
 from models.cliente import Cliente, ClienteDAO
 from models.servico import Servico, ServicoDAO
+from models.horario import Horario, HorarioDAO
 
 class View:
     def cliente_listar():
         return ClienteDAO.listar()
-    
+
+    def cliente_listar_id(id):
+        return ClienteDAO.listar_id(id)
+
     def cliente_inserir(nome, email, fone):
         cliente = Cliente(0, nome, email, fone)
 
@@ -21,6 +25,9 @@ class View:
 
     def servico_listar():
         return ServicoDAO.listar()
+
+    def servico_listar_id(id):
+        return ServicoDAO.listar_id(id)
     
     def servico_inserir(descricao, valor):
         servico = Servico(0, descricao, valor)
@@ -35,3 +42,24 @@ class View:
         servico = Servico(id, "", "")
 
         ServicoDAO.excluir(servico)
+    
+    def horario_inserir(data, confirmado, id_cliente, id_servico):
+        c = Horario(0, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.inserir(c)
+
+    def horario_listar():
+        return HorarioDAO.listar()
+    
+    def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
+        c = Horario(id, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.atualizar(c)
+
+    def horario_excluir(id):
+        c = Horario(id, None)
+        HorarioDAO.excluir(c)
