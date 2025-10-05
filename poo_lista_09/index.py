96afe3a463330a1334009f6f6ea22582f200d9f9
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2025-10-05 09:26:09 -0300

    mod

--- poo_lista_09/index.py
+++ poo_lista_09/index.py
@@ -1,7 +1,12 @@
 from templates.manterclienteUI import ManterClienteUI
 from templates.manterservicoUI import ManterServicoUI
 from templates.manterhorarioUI import ManterHorarioUI
+from templates.
+from templates.abrircontaUI import AbrirContaUI
+from templates.loginUI import LoginUI
+from templates.perfilclienteUI import PerfilClienteUI
 import streamlit as st
+from views import View
 
 class IndexUI:
     def menu_admin():
@@ -9,9 +14,29 @@ class IndexUI:
         if op == "Cadastro de Clientes": ManterClienteUI.main()
         if op == "Cadastro de Serviços": ManterServicoUI.main()
         if op == "Cadastro de Horários": ManterHorarioUI.main()
+        
     def sidebar():
         IndexUI.menu_admin()
+
+    def menu_visitante():
+        op = st.sidebar.selectbox("Menu", ["Entrar no Sistema", "Abrir Conta"])
+        if op == "Entrar no Sistema": LoginUI.main()
+        if op == "Abrir Conta": AbrirContaUI.main()
+
+    def menu_cliente():
+        op = st.sidebar.selectbox("Menu", ["Meus Dados"])
+        if op == "Meus Dados": PerfilClienteUI.main()
+
+    def menu_admin():
+        op = st.sidebar.selectbox("Menu", ["Cadastro de Clientes", "Cadastro de Serviços", "Cadastro de Horários", "Cadastro de Profissionais"])
+        if op == "Cadastro de Clientes": ManterClienteUI.main()
+        if op == "Cadastro de Serviços": ManterServicoUI.main()
+        if op == "Cadastro de Horários": ManterHorarioUI.main()
+        if op == "Cadastro de Profissionais": ManterProfissionalUI.main()
+
     def main():
+        View.cliente_criar_admin()
         IndexUI.sidebar()
+
 
 IndexUI.main()
